e2d5665d8fea6f25a57ef57a8f611c02b265879a
Amanda Diniz <amandadinizpod@gmail.com>
2024-11-10 10:28:21 -0300

    feat(backend): create article sereliazer

---
+++ backend/news/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from news.models import Article
+
+class PostSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+    author = serializers.CharField(source="author.username")
+    
+    class Meta:
+        model = Article
+        fields = [
+            "id",
+            "title",
+            "subtitle",
+            "image",
+            "content",
+            "author",
+            "publish_date",
+            "create_date",
+        ]
